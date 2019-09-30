2e3ecaa57c44041931a20fae8e0aa0639d3fad9e
Ashwanimvit16 <singh97ash@gmail.com>
2019-10-01 03:33:43 +0530

    pattern2

--- CPP/pattern.cpp
+++ CPP/pattern.cpp
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 int main()
 {
     int i, j, rows;
     printf("Enter number of rows: ");
-    scanf("%d",&rows);
+    cin>>rows;
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
